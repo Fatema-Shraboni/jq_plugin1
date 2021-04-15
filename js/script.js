566a2635142616064aeac47c53ed9d3e43c80196
Kaniz Fatema <77629494+Fatema-Shraboni@users.noreply.github.com>
2021-04-15 13:04:52 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+ // counter
+ $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+});
+
+// wow.js
+  wow = new WOW(
+        {
+           
+          mobile:false,
+           
+            }
+        )
+     wow.init();
